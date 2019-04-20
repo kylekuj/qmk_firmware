139daff43fde66cc842591374027a9f7a391c5fe
William Chang <william@factual.com>
2019-04-20 12:09:47 -0700

    Initial commit

--- keyboards/dz60/keymaps/billiams/build_flash.sh
+++ keyboards/dz60/keymaps/billiams/build_flash.sh
@@ -2,8 +2,14 @@
 # dfu-programmer atmega32u4 flash /path/to/firmware.hex
 # dfu-programmer atmega32u4 reset
 
+<<<<<<< HEAD
 # run this in the qmk_firmware directory
 make dz60:billiams
 dfu-programmer atmega32u4 erase --force && \
 dfu-programmer atmega32u4 flash dz60_billiams.hex && \
+=======
+make dz60:billiams
+dfu-programmer atmega32u4 erase --force && \
+dfu-programmer atmega32u4 flash .build/dz60_billiams.hex && \
+>>>>>>> 156fd4e96... Initial commit
 dfu-programmer atmega32u4 reset
